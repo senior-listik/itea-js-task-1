3b4c66474278e3ebd217ec7bddb7550cef471ab3
Max Lystopadov <senior.listik@gmail.com>
2023-01-25 15:55:33 +0200

    create

---
+++ js/index.js
@@ -0,0 +1,16 @@
+// let array = [2, 3, 5];
+// console.log(array.length);
+
+// var a = 5;
+// var b = 3;
+// var c;
+// document.write(a, b);
+// c = a;
+// a = b;
+// b = c;
+
+// document.write(a, b);
+
+var a = +prompt('How much?');
+var b = 5 + a;
+document.write(b);
