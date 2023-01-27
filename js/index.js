09a1dc30e0fb1653d43b7f5fe27ab7ef96a410f0
Max Lystopadov <senior.listik@gmail.com>
2023-01-27 16:40:34 +0200

    Homework done

--- js/index.js
+++ js/index.js
@@ -1,16 +1,42 @@
-// let array = [2, 3, 5];
-// console.log(array.length);
 
-// var a = 5;
-// var b = 3;
-// var c;
-// document.write(a, b);
-// c = a;
-// a = b;
-// b = c;
+// 2. Обменять значения двух переменных, используя третью (буферную) переменную.
 
-// document.write(a, b);
+var a = 5;
+var b = 3;
+var c;
+document.write(a, b);
+c = a;
+a = b;
+b = c;
+document.write(a, b);
 
-var a = +prompt('How much?');
-var b = 5 + a;
-document.write(b);
+
+// 3. Объявите две переменные: admin и name. Запишите в admin строку "Василий". Замените значение из admin в name. Выведите name.
+    
+var admin; 
+var name;
+admin = 'Василий';
+name = admin;
+document.write(name);
+
+
+// 4. Получить от пользователя данные о пользователе: имя, фамилию, возраст. Обработать их и вывести в консоль.
+
+var name = prompt('Enter your name');
+var lastName = prompt('Enter your last name');
+var age = prompt('Enter your age');
+
+console.log(`"Congratulations!!!" Your full name: ${name} ${lastName}, and you are ${age} years old`);
+
+    
+// 5. Запросите у пользователя его возраст, после чего выведите на экран модальное окно с вопросом: "Ваш возраст ...(введеное пользователем число) лет?".
+// Далее в зависимости от выбора пользователя выводится модальное окно с информацией true или false.
+
+var age = prompt('Enter your age');
+var answer = confirm(`Ваш возраст ${age} лет?`);
+if (answer == true) {
+    alert('TRUE');
+} else {
+    alert('FALSE');
+}
+
